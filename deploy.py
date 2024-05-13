c22e79170e5107983253a050ffde5aef0aa8792a
viktoria00111 <123251981+viktoria00111@users.noreply.github.com>
2024-05-13 12:13:36 +0300

    dz

---
+++ deploy.py
@@ -0,0 +1,17 @@
+from sshcheckers import ssh_checkout, upload_files
+
+
+def deploy():
+    res = []
+    upload_files("0.0.0.0", "user2", "12345", "test/p7zip-full.deb", "/home/user2/p7zip-full.deb")
+    res.append(ssh_checkout("0.0.0.0", "user2", "12345", "echo '12345' | sudo -S dpkg -i /home/user2/p7zip-full.deb",
+                            "Настраивается пакет"))
+    res.append(ssh_checkout("0.0.0.0", "usr2", "12345", "echo '12345' | sudo -S dpkg -s p7zip-full",
+                            "Status: install ok installed"))
+    return all(res)
+
+
+if deploy():
+    print("Деплой успешен.")
+else:
+    print("Деплой не удался!")
